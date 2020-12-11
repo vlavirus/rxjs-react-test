dac899cf2cb5d5cd2c13d46dad3069f375b352d0
Andriy <androando777@gmail.com>
2020-12-11 12:37:30 +0200

    create viewer

---
+++ src/Viewer/Viewer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import style from './Viewer.module.css'
+
+const Viewer = () => {
+    return (
+        <section>
+            <div className={style.wrapper}>
+                <h1>Hello!</h1>
+            </div>
+        </section>
+    )
+}
+
+export default Viewer;
---
+++ src/Viewer/ViewerContainer.jsx
@@ -0,0 +1,12 @@
+import React, {useEffect, useReducer} from 'react'
+import Viewer from './Viewer'
+
+
+
+const ViewerContainer = () => {
+    return (
+        <Viewer/>
+    )
+}
+
+export default ViewerContainer
